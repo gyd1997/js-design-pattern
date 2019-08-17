9529ec8fa4a05fd8166d9bdb1fcf224d2ee89fe4
gyd1997 <gyd0008@163.com>
2019-08-17 10:45:05 +0800

    project init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    eat() {
+        alert(`${this.name} eat something`)
+    }
+    speak() {
+        alert(`My name is ${this.name}, age ${this.age}`)
+    }
+}
+
+let zhang = new Person('zhang', 20)
+zhang.eat()
+zhang.speak()
+
+let wang = new Person('wang', 21)
+wang.eat()
+wang.speak()
